3e59d0c6102ab3766ee4a8aaf3b4bfc7493a0e5b
Tomasz Zawiślak <tomasz.zawislak.stud@pw.edu.pl>
2026-04-25 11:07:43 +0200

    ch01 done

--- chapter-01.py
+++ chapter-01.py
@@ -1,22 +1,26 @@
 from py_ecc.bn128 import is_on_curve, FQ
 from py_ecc.fields import field_properties
-field_mod = field_properties["bn128"]["field_modulus"]
 from hashlib import sha256
 from libnum import has_sqrtmod_prime_power, sqrtmod_prime_power
 
-b = 3 # for bn128, y^2 = x^3 + 3
-seed = "RareSkills"
 
+field_mod = field_properties["bn128"]["field_modulus"]
+
+b = 3 # for bn128, y^2 = x^3 + 3
+seed = "RareSkills_seed"
+
+n = 4
+# we intend to generate n EC points that will serve the Pedersen commitment scheme
+# Setp 1: 
+# calculate a random field element
 x = int(sha256(seed.encode('ascii')).hexdigest(), 16) % field_mod 
 
 entropy = 0
 
 vector_basis = []
 
-# modify the code below to generate n points
-
-n = 4
 for nn in range(n):
+    # Step 2: check if the generated field element belongs to the curve
     while not has_sqrtmod_prime_power((x**3 + b) % field_mod, field_mod, 1):
         # increment x, so hopefully we are on the curve
         x = (x + 1) % field_mod
@@ -28,6 +32,6 @@ for nn in range(n):
     assert is_on_curve(point, b), "sanity check"
     vector_basis.append(point)
 
-# new x value
+# new x value, for another basis generation
 x = int(sha256(str(x).encode('ascii')).hexdigest(), 16) % field_mod 
-print(vector_basis)
+print(f"Pedersen commitment basis: \n {vector_basis}")
